4f7671e03d32d63f08c819240fe9d50e7dc347b1
jacob <jacobharv00@gmail.com>
2021-07-21 16:43:43 -0600

    Messin shit up

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,5 @@
 require "pry"
+
 class Person
     attr_accessor :bank_account
     attr_reader :name, :happiness, :hygiene
@@ -11,17 +12,61 @@ class Person
     end
     
     def happiness(happy_points)
-        if happy_points <= 10 && happy_points >= 0 
+      @happiness = if happy_points > 10 
+        10  
+      elsif  happy_points < 0 
+        0
+      else 
         happy_points
-       end
+      end
     end
 
-    def got_money(amount)
+    def hygiene(hygiene_points)
+      @hygiene = if hygiene_points > 10 
+        10  
+      elsif  hygiene_points < 0 
+        0
+      else 
+        hygiene_points
+      end
+    end
+
+    def get_money(amount)
         self.bank_account += amount
+        "all about the benjamins"
     end
 
-person = Person.new("Ivan")
-    person.happiness(11)
-    person.got_money(50)
+    def clean?
+      @hygiene > 7 
+    end
+
+    def happy?
+      @happiness > 7
+    end
+
+    def take_bath(hygiene)
+      @hygiene << self
+      "Rub-a-dub just relaxing in the tub"
+    end
+    
+    
+  end
+  
+  ivan = Person.new("Ivan")
+  ivan.get_money(50)
+  ivan.happiness(8)
+  
+
+
+
+
+
+
+
+
+
+
+
+
 binding.pry
-end
+0
