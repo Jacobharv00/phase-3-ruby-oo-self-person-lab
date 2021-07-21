699a1a40d40915bb61069494a6c380a2af669bf9
Ivan <ivanwiesner@gmail.com>
2021-07-21 15:36:42 -0600

    change driving

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,27 @@
-# your code goes here
-update
+require "pry"
+class Person
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+    
+    def happiness(happy_points)
+        if happy_points <= 10 && happy_points >= 0 
+        happy_points
+       end
+    end
+
+    def got_money(amount)
+        self.bank_account += amount
+    end
+
+person = Person.new("Ivan")
+    person.happiness(11)
+    person.got_money(50)
+binding.pry
+end
